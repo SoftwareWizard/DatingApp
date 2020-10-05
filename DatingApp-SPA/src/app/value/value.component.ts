0fa7a64717879e5d55fe61218cb72ebe230a9c8b
Simon Bauer <simon.bauer@doka.com>
2020-10-05 14:10:54 +0200

    implement backend-access

--- DatingApp-SPA/src/app/value/value.component.ts
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -1,22 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
+export interface Value {
+   id: number;
+   name: string;
+}
+
 @Component({
    selector: 'app-value',
    templateUrl: './value.component.html',
    styleUrls: ['./value.component.css'],
 })
 export class ValueComponent implements OnInit {
-   values: any;
+   values: Value[];
 
    constructor(private http: HttpClient) {}
 
-   ngOnInit() {
-     this.getValues();
+   ngOnInit(): void {
+      this.getValues();
    }
 
-   getValues() {
-      this.http.get('http://localhost:5000/api/values').subscribe(
+   getValues(): void {
+      this.http.get<Value[]>('http://localhost:5000/api/values').subscribe(
          response => {
             this.values = response;
          },
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -21,8 +21,10 @@ namespace DatingApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             var connectionString = Configuration.GetConnectionString("DefaultConnection");
+
             services.AddDbContext<DataContext>(optionsBuilder => optionsBuilder.UseSqlServer(connectionString));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +36,11 @@ namespace DatingApp.API
             }
 
             app.UseRouting();
+
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod());
 
             app.UseAuthorization();
 
