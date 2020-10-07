1b599608c31ff5c2f06841cfb79360cd0075b187
Simon Bauer <simon.bauer@doka.com>
2020-10-07 12:37:18 +0200

    implement ExceptionHandlingMiddleware

---
+++ DatingApp.API/Controllers/BuggyController.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuggyController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public BuggyController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "secret text";
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult<string> GetNotFound()
+        {
+            return NotFound();
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+            return StatusCode((int)HttpStatusCode.InternalServerError);
+        }
+
+        [HttpGet("null-reference-exception")]
+        public ActionResult<string> GetNullReferenceException()
+        {
+            var user = _context.Users.Find(-1);
+            return user.Username;
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("This was not good");
+        }
+    }
+}
