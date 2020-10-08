f11450390f7237e3f40719fad30d73ffecd1c88e
Simon Bauer <simon.bauer@doka.com>
2020-10-08 07:54:45 +0200

    extend user

--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -1,4 +1,9 @@
-﻿namespace DatingApp.API.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using DatingApp.API.Extensions;
+
+namespace DatingApp.API.Models
 {
     public class User
     {
@@ -6,5 +11,19 @@
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string Looking { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+
+        [NotMapped]
+        public int Age => DateOfBirth.CalculateAge();
     }
 }
