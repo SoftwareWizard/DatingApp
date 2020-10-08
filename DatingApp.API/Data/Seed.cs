27d4430f27aca55d4b6658eadefdeaaed3616232
Simon Bauer <simon.bauer@doka.com>
2020-10-08 08:31:14 +0200

    add seeding

---
+++ DatingApp.API/Data/Seed.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class Seed
+    {
+        private const string FILE_PATH = "Data/UserSeedData.json";
+
+        public static async Task SeedUsers(DataContext context)
+        {
+            var hasUsers = await context.Users.AnyAsync();
+
+            if (hasUsers)
+            {
+                return;
+            }
+
+            var userData = await File.ReadAllTextAsync(FILE_PATH);
+            var users = JsonSerializer.Deserialize<List<User>>(userData);
+
+            foreach (var user in users)
+            {
+                using var hmac = new HMACSHA512();
+                user.Username = user.Username.ToLower();
+                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
+                user.PasswordSalt = hmac.Key;
+            }
+
+            await context.Users.AddRangeAsync(users);
+            await context.SaveChangesAsync();
+        }
+    }
+}
