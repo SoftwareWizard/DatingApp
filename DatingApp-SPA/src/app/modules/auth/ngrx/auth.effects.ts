c097dfacfe18874451053250779b3893e03ba373
Simon Bauer <simon.bauer@doka.com>
2020-10-16 13:15:37 +0200

    another working solution

--- DatingApp-SPA/src/app/modules/auth/ngrx/auth.effects.ts
+++ DatingApp-SPA/src/app/modules/auth/ngrx/auth.effects.ts
@@ -1,39 +1,27 @@
 import { AccountService } from './../services/account.service';
 import { Injectable } from '@angular/core';
-import { Actions, createEffect, Effect, EffectConfig, ofType } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 import * as AuthActions from './auth.actions';
-import { catchError, map, tap } from 'rxjs/operators';
-import { Store } from '@ngrx/store';
-import { AuthState } from './auth.reducer';
+import { catchError, exhaustMap, map, tap } from 'rxjs/operators';
 import { of } from 'rxjs';
 
 @Injectable({
    providedIn: 'root',
 })
 export class AuthEffects {
-   constructor(
-      private actions$: Actions,
-      private accountService: AccountService,
-      private authStore: Store<AuthState>
-   ) {}
+   constructor(private actions$: Actions, private accountService: AccountService) {}
 
-   navbarLogin$ = createEffect(
-      () => {
-         return this.actions$.pipe(
-            ofType(AuthActions.navbarLogin),
-            map(action =>
-               this.accountService
-                  .login(action.loginModel)
-                  .pipe(
-                     map(user => this.authStore.dispatch(AuthActions.loginSuccess({ user }))),
-                     catchError(error => of(AuthActions.loginFailed({ error })))
-                  )
-                  .subscribe()
+   navbarLogin$ = createEffect(() => {
+      return this.actions$.pipe(
+         ofType(AuthActions.navbarLogin),
+         exhaustMap(action =>
+            this.accountService.login(action.loginModel).pipe(
+               map(user => AuthActions.loginSuccess({ user })),
+               catchError(error => of(AuthActions.loginFailed({ error })))
             )
-         );
-      },
-      { dispatch: false }
-   );
+         )
+      );
+   });
 
    loginSuccess$ = createEffect(
       () => {
@@ -50,6 +38,16 @@ export class AuthEffects {
          return this.actions$.pipe(
             ofType(AuthActions.loginFailed),
             tap(error => console.log(error))
+         );
+      },
+      { dispatch: false }
+   );
+
+   navbarLogout$ = createEffect(
+      () => {
+         return this.actions$.pipe(
+            ofType(AuthActions.navbarLogout),
+            tap(() => console.log('navbar logout effect'))
          );
       },
       { dispatch: false }
