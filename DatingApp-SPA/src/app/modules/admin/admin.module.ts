eaac4fb8f57362030b28d664fdf9a7fa1f491f49
Simon Bauer <simon.bauer@doka.com>
2020-10-21 12:28:06 +0200

    add components

--- DatingApp-SPA/src/app/modules/admin/admin.module.ts
+++ DatingApp-SPA/src/app/modules/admin/admin.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminPanelComponent } from './components/admin-panel/admin-panel.component';
 import { HasRoleDirective } from './directives/has-role.directive';
+import { UserManagementComponent } from './components/user-management/user-management.component';
+import { PhotoManagementComponent } from './components/photo-management/photo-management.component';
 
 @NgModule({
-   declarations: [AdminPanelComponent, HasRoleDirective],
+   declarations: [AdminPanelComponent, HasRoleDirective, UserManagementComponent, PhotoManagementComponent],
    imports: [CommonModule, AdminRoutingModule],
    exports: [HasRoleDirective]
 })
