af463b373d5ec12e0f0f5e2605f83ba2960202c1
Simon Bauer <simon.bauer@doka.com>
2020-11-05 17:49:25 +0100

    cli configuration

--- DatingApp-SPA/karma.conf.js
+++ DatingApp-SPA/karma.conf.js
@@ -15,12 +15,13 @@ module.exports = function (config) {
       client: {
          clearContext: false, // leave Jasmine Spec Runner output visible in browser
       },
+      reporters: ['progress', 'kjhtml','coverage-istanbul'],
       coverageIstanbulReporter: {
-         dir: require('path').join(__dirname, './coverage/DatingApp-SPA'),
+         dir: require('path').join(__dirname, './coverage'),
          reports: ['html', 'lcovonly', 'text-summary'],
          fixWebpackSourcePaths: true,
+         skipFilesWithNoCoverage: false
       },
-      reporters: ['progress', 'kjhtml'],
       port: 9876,
       colors: true,
       logLevel: config.LOG_INFO,
