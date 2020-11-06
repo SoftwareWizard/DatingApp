0324e48a8fad1eb4e92c22a883ee60d4b21ef305
Simon Bauer <simon.bauer@doka.com>
2020-11-06 12:06:20 +0100

    implement likes.container.spec

---
+++ DatingApp-SPA/src/app/core/container/likes/likes.container.spec.ts
@@ -0,0 +1,51 @@
+import { LikedPredicateType } from 'src/app/modules/members/models/likedPredicate.type';
+import { TEST_MEMBERS } from 'test/test-members';
+import { Spectator, createComponentFactory } from '@ngneat/spectator';
+import { LikesContainerComponent } from './likes.container';
+import { LikeService } from 'src/app/modules/members';
+import { of } from 'rxjs';
+
+describe('LikesContainerComponent', () => {
+   let spectator: Spectator<LikesContainerComponent>;
+   const createComponent = createComponentFactory({
+      component: LikesContainerComponent,
+      mocks: [LikeService],
+   });
+
+   beforeEach(() => (spectator = createComponent()));
+
+   it('should ', () => {
+      expect(spectator.component).toBeTruthy();
+   });
+
+   describe('loadLikes', () => {
+      it('should return members', async () => {
+         const likeServiceSpy = spectator.inject(LikeService);
+         likeServiceSpy.getLikes.and.returnValue(of(TEST_MEMBERS));
+
+         await spectator.component.loadLikes();
+         const result = spectator.component.members;
+         expect(result.length).toBe(TEST_MEMBERS.length);
+         expect(likeServiceSpy.getLikes).toHaveBeenCalledWith(
+            LikedPredicateType.liked
+         );
+      });
+   });
+
+   describe('onPredicateChanged', () => {
+      it('should return members', async () => {
+         const likeServiceSpy = spectator.inject(LikeService);
+         likeServiceSpy.getLikes.and.returnValue(of(TEST_MEMBERS));
+
+         await spectator.component.onPredicateChanged(
+            LikedPredicateType.likedBy
+         );
+
+         const result = spectator.component.members;
+         expect(result.length).toBe(TEST_MEMBERS.length);
+         expect(likeServiceSpy.getLikes).toHaveBeenCalledWith(
+            LikedPredicateType.likedBy
+         );
+      });
+   });
+});
